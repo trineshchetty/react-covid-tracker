869db00c19024a465cda409abac8fd803704f4c2
Trinesh <trinesh.chetty@sintrex.com>
2020-05-22 17:05:43 +0200

    test

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -3,6 +3,7 @@ import { fetchDailyData } from "../../api"
 import {Line, Bar} from "react-chartjs-2"
 import styles from "./Chart.module.css"
 
+// new test
 const Charts = ({data: {confirmed, recovered, deaths}, country}) => {
     const [dailyData, setDailyData] = useState([])
 
